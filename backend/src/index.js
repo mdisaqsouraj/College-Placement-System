5d7cdba7f857127d3401b871a5de53302a703e0d
Md Isaq Souraj <sourajmdisaq682@gmail.com>
2026-08-16 10:16:08 +0530

    Backed Server Established Succesfully

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/",(req,res)=>{
+    res.send("College-Placement-System server is running sucessfully")
+})
+app.listen(PORT,()=>{
+    console.log(`App is Running On PORT number: ${PORT}`);
+})
